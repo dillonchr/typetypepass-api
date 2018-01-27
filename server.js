ba3dc9323ea3d8e9cf08f38dfbf0225b6c7192fd
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-01-26 20:37:12 -0600

    still debugging, but trying to get refreshes to work

--- server.js
+++ server.js
@@ -14,19 +14,35 @@ const game = require('./store');
 app.listen(process.env.PORT || 3000);
 
 io.on('connection', socket => {
+    const user = {id: null, removePlayerTimer: null};
+
+    socket.on('register-uuid', uuid => {
+        user.id = uuid;
+        game.addPlayer(uuid, socket);
+        console.log('connected', uuid);
+    });
+
     socket.on('add-player', name => {
-        game.addPlayer(name, socket);
+        game.namePlayer(user.id, name);
+        console.log(user.id, name);
         io.emit('list-players', game.getPlayerList());
         if (game.canStart()) {
+            console.log('SME1STARTIN');
             game.begin();
             game.getPlayerSocket(0).emit('your-turn', {first: true});
+        } else if (game.playerIsNext(user.id)) {
+            console.log('HIS TURN', name);
+            socket.emit('your-turn', game.getPromptForNextPlayer());
+        } else {
+            console.log('WAIT A SEC', name);
+            socket.emit('wait');
         }
     });
 
     socket.on('add-line', line => {
         if (game.isStarted()) {
-            const nextSocket = game.addLine(line, socket);
-            nextSocket.emit('your-turn', {prompt: line, cycle: game.getCycle()});
+            const nextSocket = game.addLine(line, user.id);
+            nextSocket.emit('your-turn', game.getPromptForNextPlayer());
         } else {
             socket.emit('early-bird');
         }
@@ -34,7 +50,7 @@ io.on('connection', socket => {
 
     socket.on('end-story', line => {
         if (game.isStarted()) {
-            game.addLine(line, socket);
+            game.addLine(line, user.id);
             io.emit('storytime', game.getStory());
             game.reset();
         } else {
@@ -43,7 +59,11 @@ io.on('connection', socket => {
     });
 
     socket.on('disconnect', () => {
-        game.removePlayer(socket);
+        game.disconnectPlayer(user.id);
+    });
+
+    socket.on('exit-game', () => {
+        game.removePlayer(user.uuid);
         io.emit('list-players', game.getPlayerList());
     });
 });
--- store.js
+++ store.js
@@ -2,25 +2,43 @@ let players = [];
 let lines = [];
 let started = false;
 let cycle = 1;
+let removePlayerTimers = {};
 
 module.exports = {
-    addPlayer(name, socket) {
-        players = players.filter(p => p.socket !== socket).filter(p => p.name !== name);
-        players = [...players, {name, socket}];
+    addPlayer(uuid, socket) {
+        if (!players.some(p => p.uuid === uuid)) {
+            const upNext = !players.length;
+            players = [...players, {uuid, socket, upNext}];
+        } else {
+            clearTimeout(removePlayerTimers[uuid]);
+            delete removePlayerTimers[uuid];
+        }
     },
-    removePlayer(socket) {
-        players = players.filter(p => p.socket !== socket);
+    playerIsNext(uuid) {
+        return started && players.some(p => p.uuid === uuid && p.upNext);
+    },
+    removePlayer(uuid) {
+        console.log('REMOVING', uuid);
+        players = players.filter(p => p.uuid !== uuid);
+    },
+    disconnectPlayer(uuid) {
+        removePlayerTimers[uuid] = setTimeout(() => this.removePlayer(uuid), 10000);
+    },
+    namePlayer(uuid, name) {
+        players = players.map(p => p.uuid === uuid ? {...p, name} : p);
     },
     getPlayerList() {
+        console.log(players.map(p => p.name));
         return players.map(p => p.name);
     },
     getPlayerSocket(index) {
         return players[index].socket;
     },
-    addLine(line, fromSocket) {
+    addLine(line, uuid) {
         lines = [...lines, line];
-        const submittorIndex = players.findIndex(p => p.socket === fromSocket);
+        const submittorIndex = players.findIndex(p => p.uuid === uuid);
         const nextIndex = submittorIndex === players.length - 1 ? 0 : submittorIndex + 1;
+        players = players.map((p, i) => ({...p, upNext: i === nextIndex}))
         if (!nextIndex) {
             cycle++;
         }
@@ -28,6 +46,9 @@ module.exports = {
     },
     getStory() {
         return lines.join(' ');
+    },
+    getPromptForNextPlayer() {
+        return {prompt: lines.slice(-1).shift(), cycle};
     },
     getCycle() {
         return cycle;
