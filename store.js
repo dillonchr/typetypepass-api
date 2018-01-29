a1b5e1321d78cc82d9859dcac0406a93d48f2abf
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-01-29 16:35:12 -0600

    log heavy, but works with refresh pretty well, some inconsistencies but better than before

--- store.js
+++ store.js
@@ -5,11 +5,18 @@ let cycle = 1;
 let removePlayerTimers = {};
 
 module.exports = {
+    logState(...preMessage) {
+        console.log(preMessage, players.map(p => p.name).join(), `#oLINES: ${lines.length}; #cycle: ${cycle}; #started: ${started}`);
+    },
     addPlayer(uuid, socket) {
+        this.logState(uuid, 'IS IT IN');
         if (!players.some(p => p.uuid === uuid)) {
+            this.logState('NOpe', uuid, 'is new');
             const upNext = !players.length;
-            players = [...players, {uuid, socket, upNext}];
+            players = [...players, {uuid, socket, upNext, online: true}];
         } else {
+            this.logState('Yes, updating', uuid);
+            players = players.map(p => p.uuid === uuid ? {...p, socket, online: true} : p);
             clearTimeout(removePlayerTimers[uuid]);
             delete removePlayerTimers[uuid];
         }
@@ -18,17 +25,17 @@ module.exports = {
         return started && players.some(p => p.uuid === uuid && p.upNext);
     },
     removePlayer(uuid) {
-        console.log('REMOVING', uuid);
-        players = players.filter(p => p.uuid !== uuid);
+        players = players.filter(p => p.online || p.uuid !== uuid);
+        this.logState('TRIED TO REMOVING', uuid);
     },
     disconnectPlayer(uuid) {
+        players = players.map(p => p.uuid === uuid ? {...p, online: false} : p);
         removePlayerTimers[uuid] = setTimeout(() => this.removePlayer(uuid), 10000);
     },
     namePlayer(uuid, name) {
         players = players.map(p => p.uuid === uuid ? {...p, name} : p);
     },
     getPlayerList() {
-        console.log(players.map(p => p.name));
         return players.map(p => p.name);
     },
     getPlayerSocket(index) {
@@ -42,6 +49,7 @@ module.exports = {
         if (!nextIndex) {
             cycle++;
         }
+        this.logState(nextIndex, 'SENDING LINE', line, 'TO', players[nextIndex].name);
         return players[nextIndex].socket;
     },
     getStory() {
@@ -63,6 +71,7 @@ module.exports = {
         started = true;
     },
     reset() {
+        players.length = 0;
         lines.length = 0;
         started = false;
         cycle = 1;
