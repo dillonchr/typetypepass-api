3099f5378d7403ba2649c799300dbf0a0a5a6473
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-01-29 19:50:38 -0600

    ready to roll baby

--- server.js
+++ server.js
@@ -16,27 +16,33 @@ app.listen(process.env.PORT || 3000);
 io.on('connection', socket => {
     const user = {id: null, removePlayerTimer: null};
 
+    const welcomeNewPlayer = uuid => {
+        io.emit('list-players', game.getPlayerList());
+        if (game.canStart()) {
+            game.begin();
+            game.getPlayerSocket(0).emit('your-turn', {first: true});
+        } else if (game.playerIsNext(user.id)) {
+            socket.emit('your-turn', game.getPromptForNextPlayer());
+        } else {
+            socket.emit('wait');
+        }
+    };
+
     socket.on('register-uuid', uuid => {
         user.id = uuid;
         game.addPlayer(uuid, socket);
-        console.log('connected', uuid);
+    });
+
+    socket.on('restart', ({name, uuid}) => {
+        user.id = uuid;
+        game.addPlayer(uuid, socket);
+        game.namePlayer(uuid, name);
+        welcomeNewPlayer(uuid);
     });
 
     socket.on('add-player', name => {
         game.namePlayer(user.id, name);
-        console.log(user.id, name);
-        io.emit('list-players', game.getPlayerList());
-        if (game.canStart()) {
-            console.log('SME1STARTIN');
-            game.begin();
-            game.getPlayerSocket(0).emit('your-turn', {first: true});
-        } else if (game.playerIsNext(user.id)) {
-            console.log('HIS TURN', name);
-            socket.emit('your-turn', game.getPromptForNextPlayer());
-        } else {
-            console.log('WAIT A SEC', name);
-            socket.emit('wait');
-        }
+        welcomeNewPlayer(user.id);
     });
 
     socket.on('add-line', line => {
