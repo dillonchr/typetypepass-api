ec2ba2b61ac0663d000bba18f2d1de190c04b49f
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-01-30 16:05:55 -0600

    Revert "actually made a server to serve the frontend content from another dir"
    This reverts commit e3dbb743afbfeb0f0c2b1066a8b34912aa88f89e.

--- server.js
+++ server.js
@@ -1,43 +1,75 @@
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
-const mimeTypes = {
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.css': 'text/css',
-    '.json': 'application/json',
-    '.map': 'application/json',
-    '.png': 'image/png',
-    '.jpg': 'image/jpg',
-    '.gif': 'image/gif',
-    '.wav': 'audio/wav',
-    '.mp4': 'video/mp4',
-    '.woff': 'application/font-woff',
-    '.ttf': 'application/font-ttf',
-    '.eot': 'application/vnd.ms-fontobject',
-    '.otf': 'application/font-otf',
-    '.svg': 'application/image/svg+xml'
-};
 const app = https.createServer({
-    key: fs.readFileSync(process.env.SSL_KEY_PATH),
-    cert: fs.readFileSync(process.env.SSL_CERT_PATH)
+    key: fs.readFileSync(path.join(process.env.CERTS_PATH, 'dillonchristensen.com.key')),
+    cert: fs.readFileSync(path.join(process.env.CERTS_PATH, 'dillonchristensen.com.cer'))
 }, (req, res) => {
-    const filePath = path.join(process.env.FRONT_END_DIR, req.url === '/' ? '/index.html' : req.url);
-    const contentType = mimeTypes[path.extname(filePath).toLowerCase()] || 'application/octet-stream';
-    fs.readFile(filePath, (error, content) => {
-        if (error) {
-            if(error.code === 'ENOENT'){
-                res.writeHead(404, {'Content-Type': 'text/plain'});
-                res.end('404', 'utf-8');
-            } else {
-                res.writeHead(500);
-                res.end('500');
-            }
+    res.writeHead(200);
+    res.end('oh hi there', 'utf-8');
+});
+const io = require('socket.io')(app);
+const game = require('./store');
+
+app.listen(process.env.PORT || 3000);
+
+io.on('connection', socket => {
+    const user = {id: null, removePlayerTimer: null};
+
+    const welcomeNewPlayer = uuid => {
+        io.emit('list-players', game.getPlayerList());
+        if (game.canStart()) {
+            game.begin();
+            game.getPlayerSocket(0).emit('your-turn', {first: true});
+        } else if (game.playerIsNext(user.id)) {
+            socket.emit('your-turn', game.getPromptForNextPlayer());
         } else {
-            res.writeHead(200, {'Content-Type': contentType});
-            res.end(content, 'utf-8');
+            socket.emit('wait');
+        }
+    };
+
+    socket.on('register-uuid', uuid => {
+        user.id = uuid;
+        game.addPlayer(uuid, socket);
+    });
+
+    socket.on('restart', ({name, uuid}) => {
+        user.id = uuid;
+        game.addPlayer(uuid, socket);
+        game.namePlayer(uuid, name);
+        welcomeNewPlayer(uuid);
+    });
+
+    socket.on('add-player', name => {
+        game.namePlayer(user.id, name);
+        welcomeNewPlayer(user.id);
+    });
+
+    socket.on('add-line', line => {
+        if (game.isStarted()) {
+            const nextSocket = game.addLine(line, user.id);
+            nextSocket.emit('your-turn', game.getPromptForNextPlayer());
+        } else {
+            socket.emit('early-bird');
         }
     });
-}).listen(process.env.PORT || 3000);
 
-module.exports = app;
+    socket.on('end-story', line => {
+        if (game.isStarted()) {
+            game.addLine(line, user.id);
+            io.emit('storytime', game.getStory());
+            game.reset();
+        } else {
+            socket.emit('early-bird');
+        }
+    });
+
+    socket.on('disconnect', () => {
+        game.disconnectPlayer(user.id);
+    });
+
+    socket.on('exit-game', () => {
+        game.removePlayer(user.uuid);
+        io.emit('list-players', game.getPlayerList());
+    });
+});
